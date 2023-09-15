fff1bae00b4cdbb992d49dd6b94e795aafc80333
louisvanbel <louis.vanbel@student.vinci.be>
2023-09-15 18:52:47 +0200

    1.1-1.2 : refactor - modules

---
+++ exercices/module1/1.1-1.2/src/components/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+const Footer = (props) => {
+    console.log(props);
+    return (
+        <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    );
+};
+
+export default Footer;
