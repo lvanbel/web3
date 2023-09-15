16dbd7e402fb5430916223d7f76b8e035171ae8c
louisvanbel <louis.vanbel@student.vinci.be>
2023-09-15 21:34:58 +0200

    1.1-1.2 : import image

--- exercices/module1/1.1-1.2/src/components/Header/Header.jsx
+++ exercices/module1/1.1-1.2/src/components/Header/Header.jsx
@@ -1,7 +1,12 @@
+import LogoVinci from "components/Header/LOGO HE VINCI.png";
+
 const Header = (props) => {
     console.log(props);
     return (
-        <h1>{props.course}</h1>
+        <>
+            <img src={LogoVinci} width="128"></img>
+            <h1>{props.course}</h1>
+        </>
     );
 };
 
