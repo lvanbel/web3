a73297814874237c9ce982c0071dac9634bf7a0c
louisvanbel <louis.vanbel@student.vinci.be>
2024-01-07 15:50:48 +0100

    module7 : Ant Design

---
+++ exercices/module7/7.4/src/components/Anecdotes/CreateNew.jsx
@@ -0,0 +1,76 @@
+import { useState } from 'react';
+import { Button, Checkbox, Form, Input } from 'antd';
+
+const CreateNew = ({ addNew }) => {
+  const [content, setContent] = useState('');
+  const [author, setAuthor] = useState('');
+  const [info, setInfo] = useState('');
+
+  const handleSubmit = (e) => {};
+
+  const onFinish = (values) => {
+    addNew({
+      ...values,
+      votes: 0,
+    });
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };  
+
+  return (
+    <div>
+      <h2>create a new anecdote</h2>
+
+      <Form name="newAnecdote" onFinish={onFinish} onFinishFailed={onFinishFailed}>
+        <Form.Item
+          label="content"
+          name="content"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your content!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="author"
+          name="author"
+          rules={[
+            {
+              required: true,
+              message: 'Please input an author!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="url for more info"
+          name="info"
+          rules={[
+            {
+              required: true,
+              message: 'Please input a url!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            create
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
+
+export default CreateNew;
